4918aecf44c1d6de4a0eb3db2eed5b85e922cd09
Keith Wong <keith.wong3672@gmail.com>
2017-03-09 09:25:49 -0800

    Setup basic node server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', function (req, res) {
+  res.send('Node Server Running on Port: ' + port);
+});
+
+app.listen(port, function () {
+  console.log('Node Server running on port', 3000);
+});
