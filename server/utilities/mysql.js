04b514d105df158ec49814c9d137af3783794ea4
Keith Wong <keith.wong3672@gmail.com>
2017-03-14 09:25:34 -0700

    Create Articles and Videos tables in database if they do not exist

--- server/utilities/mysql.js
+++ server/utilities/mysql.js
@@ -1,26 +1,51 @@
 var mysql = require('mysql');
+var fs = require('fs');
+
 
 var db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'codefoo_backend'
+  database: 'codefoo_backend',
+  multipleStatements: true
 });
 
 db.connect(function (err) {
   if (!err) {
-    console.log('Connected to codefoo-backend');    
+    console.log('Connected to codefoo-backend');
+    loadSchema();    
   } else {
     console.log('Error connecting database');
     console.log(err);    
   }
 });
 
-db.end(function (err) {
-  if (!err) {
-    console.log('Connection to DB ended');
-  } else {
-    console.log('Error disconnecting database');
-    console.log(err);
-  }
-});
+// db.end(function (err) {
+//   if (!err) {
+//     console.log('Connection to DB ended');
+//   } else {
+//     console.log('Error disconnecting database');
+//     console.log(err);
+//   }
+// });
+
+var loadSchema = function () {
+  fs.readFile('./server/utilities/schema.sql', 'utf-8', function (err, data) {
+    if (!err) {
+      var schema = data;
+      db.query(data, function (err, response) {
+        if (!err) {
+          console.log('Successfully queried database');
+          console.log(response);
+        } else {
+          console.log('Error querying database');
+          console.log(err);
+        }
+      });
+    } else {
+      console.log('Error reading file');
+      console.log(err);
+    }
+  });
+};
+
