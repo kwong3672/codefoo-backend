6223c05e251523c5b56d0f43b18a921b16db2c45
Keith Wong <keith.wong3672@gmail.com>
2017-03-14 17:59:47 -0700

    Insert data from API to mySQL database

---
+++ server/utilities/insertDB.js
@@ -0,0 +1,57 @@
+var _ = require('lodash');
+var db = require('./mysql').db;
+
+
+var insertDB = function (category, data) {
+  console.log('*********insertDB function*************');
+
+  db.query(createInsertString(category, data), function (err, results, fields) {
+    if (!err) {
+      console.log('mySQL Insert Successful');
+    } else {
+      console.log('!!!!!!!!!!!!!!!! ERROR inserting mySQL data');
+      console.log(err);
+    }
+  }); 
+
+};
+
+var createInsertString = function (category, data) {
+  console.log(data)
+  console.log('***************createInsertString****************');
+  var insertString = [];
+
+  _.forEach(data, function (articleOrVideo) {
+    var columns = [];
+    var columnData = [];
+    _.forEach(articleOrVideo, function (heading, headingKey) {
+      
+      if (!Array.isArray(heading)) {
+        _.forEach(heading, function (metadata, metadataKey) {
+          
+          if (!Array.isArray(metadata)) {
+            columnData.push(JSON.stringify(metadata));
+          } else {
+            columnData.push(JSON.stringify(JSON.stringify(metadata)));
+          }
+
+          columns.push(metadataKey);
+          
+        });
+      } else {
+        columns.push(headingKey);
+        columnData.push(JSON.stringify(JSON.stringify(heading)));
+      }
+
+    });
+
+    insertString += 'INSERT INTO ' + category + ' (' + columns + ') VALUES (' + columnData + '); ';
+
+  });
+    
+  return insertString;
+
+};
+
+
+module.exports.insertDB = insertDB;
