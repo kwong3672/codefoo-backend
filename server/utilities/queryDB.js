fe3c0e739b19ce6407c051f8513100e760dcfde3
Keith Wong <keith.wong3672@gmail.com>
2017-03-15 16:59:36 -0700

    Create cron job to run query of DB and create RSS file every 5 minutes

--- server/utilities/queryDB.js
+++ server/utilities/queryDB.js
@@ -1,19 +1,24 @@
+var cron = require('node-cron');
 var db = require('./mysql').db;
 var createRSS = require('./rss').createRSS;
 
-var queryDB = function (req, res, category, count) {
-  db.query('SELECT * FROM ' + category + ' LIMIT 5', function (err, data) {
+var queryDB = function (category) {
+  db.query('SELECT * FROM ' + category + ' LIMIT 10', function (err, data) {
     if (!err) {
       console.log('*******queryDB.js successful**********');
       // console.log(data);
       var xml = createRSS(data, category);
-
-      res.redirect('/feeds/rss_' + category + '_feed.xml');
     } else {
       console.log('Error querying database');
-      console.log(err);
+      throw err;
     }
   });
 };
 
-module.exports.queryDB = queryDB;
+// run every 1 minute query articles and video DB tables and create RSS feed
+// would normally be larger interval like once an hour
+cron.schedule('*/1 * * * *', function () {
+  console.log('running function to query and create RSS feed every 5 minutes');
+  queryDB('articles');
+  queryDB('videos');
+});
