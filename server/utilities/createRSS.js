09940f2f48fabf24d1c0a24a5573697cc7eb5d36
Keith Wong <keith.wong3672@gmail.com>
2017-03-17 11:11:41 -0700

    Change order that functions are executed

--- server/utilities/createRSS.js
+++ server/utilities/createRSS.js
@@ -2,38 +2,44 @@ var RSS = require('rss');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
 var _ = require('lodash');
+var queryDB = require('./queryDB').queryDB;
 
-var createRSS = function (data, category) {
-  var feed = new RSS({
-    description: 'Codefoo Project to create a backend project that read/writes to a mySQL database and serves a RSS feed.',
-    site_url: 'http://localhost:3000',
-    title: 'Codefoo 2017 Backend Project',
-  });
-
-  // create feed item for each article or video
-  _.forEach(data, function (articleOrVideo) {
-    feed.item({
-      categories: [category],
-      date: articleOrVideo.publishDate, 
-      description: articleOrVideo.subheadline || articleOrVideo.description,
-      title: articleOrVideo.headline || articleOrVideo.longTitle,
-      url: '' || articleOrVideo.url
+var createRSS = function (category) {
+  
+  queryDB(category)
+  .then(function(data) {
+    var feed = new RSS({
+      description: 'Codefoo Project to create a backend project that read/writes to a mySQL database and serves a RSS feed.',
+      site_url: 'http://localhost:3000',
+      title: 'Codefoo 2017 Backend Project',
     });
-  });
 
-  var xml = feed.xml({indent: '  '});
+    // create feed item for each article or video
+    _.forEach(data, function (articleOrVideo) {
+      feed.item({
+        categories: [category],
+        date: articleOrVideo.publishDate, 
+        description: articleOrVideo.subheadline || articleOrVideo.description,
+        title: articleOrVideo.headline || articleOrVideo.longTitle,
+        url: '' || articleOrVideo.url
+      });
+    });
 
-  // save as xml files
-  var rssFileName = './public/feeds/rss_' + category + '_feed.xml';
-  fs.writeFileAsync(rssFileName, xml)
+    var xml = feed.xml({indent: '  '});
+
+    // save as xml files
+    var rssFileName = './public/feeds/rss_' + category + '_feed.xml';
+    fs.writeFileAsync(rssFileName, xml);
+  })
   .then(function() {
     console.log('Saved RSS files');
   }).catch(function(err) {
     throw err;
   });
 
-  return xml;
-  
 };
+
+createRSS('articles');
+createRSS('videos');
 
 module.exports.createRSS = createRSS;
