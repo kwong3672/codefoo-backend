f7d1f1177a5996bc96037193ea903d3b35919c34
Keith Wong <keith.wong3672@gmail.com>
2017-03-17 09:23:03 -0700

    Create Promises for async function and disconnect DB connections

--- server/utilities/queryDB.js
+++ server/utilities/queryDB.js
@@ -1,24 +1,27 @@
 var cron = require('node-cron');
-var db = require('./mysql').db;
+var Promise = require('bluebird');
+var getSqlConnection = require('./mysql').getSqlConnection;
 var createRSS = require('./createRSS').createRSS;
 
 var queryDB = function (category) {
-  db.query('SELECT * FROM ' + category + ' LIMIT 10', function (err, data) {
-    if (!err) {
-      console.log('*******queryDB.js successful**********');
-      // console.log(data);
-      var xml = createRSS(data, category);
-    } else {
-      console.log('Error querying database');
+  var queryString = 'SELECT * FROM ' + category + ' LIMIT 10';
+  Promise.using(getSqlConnection(), function(connection) {
+    connection.queryAsync(queryString)
+    .then(function(data) {
+      createRSS(data, category);
+    }).catch(function(err) {
       throw err;
-    }
+    });
   });
 };
 
 // run every 1 minute query articles and video DB tables and create RSS feed
 // would normally be larger interval like once an hour
-cron.schedule('*/1 * * * *', function () {
-  console.log('running function to query and create RSS feed every 5 minutes');
-  queryDB('articles');
-  queryDB('videos');
-});
+// cron.schedule('*/1 * * * *', function () {
+//   console.log('running function to query and create RSS feed every 5 minutes');
+//   queryDB('articles');
+//   queryDB('videos');
+// });
+
+queryDB('articles');
+queryDB('videos');
