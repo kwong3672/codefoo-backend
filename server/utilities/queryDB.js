b0f8b64df4f12bdcc6138fc3e836e929cb7e18f6
Keith Wong <keith.wong3672@gmail.com>
2017-03-17 11:10:02 -0700

    Change order that functions are executed

--- server/utilities/queryDB.js
+++ server/utilities/queryDB.js
@@ -1,27 +1,18 @@
-var cron = require('node-cron');
 var Promise = require('bluebird');
 var getSqlConnection = require('./mysql').getSqlConnection;
 var createRSS = require('./createRSS').createRSS;
 
 var queryDB = function (category) {
   var queryString = 'SELECT * FROM ' + category + ' LIMIT 10';
-  Promise.using(getSqlConnection(), function(connection) {
-    connection.queryAsync(queryString)
+  return Promise.using(getSqlConnection(), function(connection) {
+    return connection.queryAsync(queryString)
     .then(function(data) {
-      createRSS(data, category);
+      console.log('Query of ' + category + ' successful');
+      return data;
     }).catch(function(err) {
       throw err;
     });
   });
 };
 
-// run every 1 minute query articles and video DB tables and create RSS feed
-// would normally be larger interval like once an hour
-// cron.schedule('*/1 * * * *', function () {
-//   console.log('running function to query and create RSS feed every 5 minutes');
-//   queryDB('articles');
-//   queryDB('videos');
-// });
-
-queryDB('articles');
-queryDB('videos');
+module.exports.queryDB = queryDB;
